09fb5e35ae4511b7a25b429c77ba81e1e206e151
Fanny <fanny.lie@ordina.nl>
2021-03-26 16:22:02 +0100

    Fix artist detail component test

--- src/app/artists/artist-detail/artist-detail.component.spec.ts
+++ src/app/artists/artist-detail/artist-detail.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 import { ArtistDetailComponent } from './artist-detail.component';
+import { ArtistService } from '../../shared/services/artist.service'
+import { SongService }  from '../../shared/services/song.service';
 
 describe('ArtistDetailComponent', () => {
   let component: ArtistDetailComponent;
   let fixture: ComponentFixture<ArtistDetailComponent>;
+  let route: ActivatedRoute;
+  let artistService: ArtistService;
+  let songService: SongService;
 
   beforeEach(async () => {
+    component = new ArtistDetailComponent(route, artistService, songService);
+
     await TestBed.configureTestingModule({
-      declarations: [ ArtistDetailComponent ]
+      declarations: [ ArtistDetailComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      providers: [ ArtistService, SongService ]
     })
     .compileComponents();
   });
