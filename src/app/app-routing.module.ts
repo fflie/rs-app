425501d3e9c8a43c0895ef84bbbb959a97c306bf
Fanny <fanny.lie@ordina.nl>
2021-03-26 13:24:25 +0100

    Add pages and routing for artists and playlists

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistListComponent } from './artists/artist-list/artist-list.component';
+import { PlaylistListComponent } from './playlists/playlist-list/playlist-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'artist-list', component: ArtistListComponent },
+  { path: 'playlist-list', component: PlaylistListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtistListComponent } from './artists/artist-list/artist-list.component';
+import { PlaylistListComponent } from './playlists/playlist-list/playlist-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistListComponent,
+    PlaylistListComponent
   ],
   imports: [
     BrowserModule,
