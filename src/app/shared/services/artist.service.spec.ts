26f5a0b508ab39edfadbb2a621ab0d717b7fe8e6
Fanny <fanny.lie@ordina.nl>
2021-03-26 15:20:19 +0100

    Fix some tests

--- src/app/shared/services/artist.service.spec.ts
+++ src/app/shared/services/artist.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ArtistService } from './artist.service';
 
 describe('ArtistService', () => {
   let service: ArtistService;
+  let serviceSpy: jasmine.SpyObj<ArtistService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('ArtistService', ['getValue']);
+
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ], 
+      providers: [
+        ArtistService,
+        { provide: ArtistService, useValue: spy }
+      ]
+    });
     service = TestBed.inject(ArtistService);
   });
 
   it('should be created', () => {
+    serviceSpy = TestBed.inject(ArtistService) as jasmine.SpyObj<ArtistService>;
     expect(service).toBeTruthy();
   });
 });
